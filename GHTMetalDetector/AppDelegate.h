98391d2f81aac3bbbbe46928e9f8d36e56be7399
perschulte <per.schulte@gmail.com>
2014-07-28 08:49:28 +0200

    Initial Commit

---
+++ GHTMetalDetector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHTMetalDetector
+//
+//  Created by Per Schulte on 28.07.14.
+//  Copyright (c) 2014 de.launchair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHTMetalDetector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHTMetalDetector
+//
+//  Created by Per Schulte on 28.07.14.
+//  Copyright (c) 2014 de.launchair. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
